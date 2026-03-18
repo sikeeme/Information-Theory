9e8a349fb5f8691cedb4c634c124c905936493e1
sikeeme <hodysdara@gmail.com>
2026-03-18 20:58:34 +0300

    add lab2

--- Lab1/Unit3.pas
+++ Lab1/Unit3.pas
@@ -41,13 +41,12 @@ Type
     Procedure OpenBtClick(Sender: TObject);
     Procedure SaveBtClick(Sender: TObject);
     Procedure ClearBtClick(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
-    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    Procedure FormCreate(Sender: TObject);
+    Procedure FormClose(Sender: TObject; Var Action: TCloseAction);
   Private
     { Private declarations }
   Public
-    IsVigenere: Boolean;
-    Encrypt: Boolean;
+    IsVigenere, Encrypt, Correct: Boolean;
     Function Playfair(Original, Key1, Key2, Key3, Key4: String): String;
     Procedure Matrix(Key: String; Var Arr: TPlayfairMatrix);
     Function Vigenere(Original, Key: String): String;
@@ -70,21 +69,21 @@ Begin
     ResM.Text := Playfair(OrM.Text, KeyFirstEd.Text, KeySecondEd.Text, KeyThirdEd.Text, KeyFourthEd.Text);
 End;
 
-procedure TForm3.FormClose(Sender: TObject; var Action: TCloseAction);
-begin
+Procedure TForm3.FormClose(Sender: TObject; Var Action: TCloseAction);
+Begin
   OrM.Clear;
   KeyFirstEd.Clear;
   KeySecondEd.Clear;
   KeyThirdEd.Clear;
   KeyFourthEd.Clear;
   ResM.Clear;
-end;
+End;
 
-procedure TForm3.FormCreate(Sender: TObject);
-begin
+Procedure TForm3.FormCreate(Sender: TObject);
+Begin
   OrM.Clear;
   ResM.Clear;
-end;
+End;
 
 Procedure TForm3.DecipherBtClick(Sender: TObject);
 Begin
@@ -110,6 +109,9 @@ Begin
       If Pos(Key[I], Temp) = 0 Then
         Temp := Temp + Key[I];
 
+  If Temp = '' Then
+    Correct := False;
+
   Alphabet := 'ABCDEFGHIKLMNOPQRSTUVWXYZ';
 
   For I := 1 To Length(Alphabet) Do
@@ -117,7 +119,6 @@ Begin
       Temp := Temp + Alphabet[I];
 
   K := 1;
-
   For I := 0 To 4 Do
     For J := 0 To 4 Do
     Begin
@@ -133,71 +134,67 @@ Var
   I, J, Row, Col, K: Integer;
   R1, C1, R2, C2: Integer;
 Begin
+  Correct := True;
   Result := '';
   Original := StringReplace(AnsiUpperCase(Original), 'J', 'I', [RfReplaceAll]);
 
-  If Encrypt Then
-  Begin
-    Matrix(Key1, First);
-    Matrix(Key2, Second);
-    Matrix(Key3, Third);
-    Matrix(Key4, Fourth);
-  End
-  Else
-  Begin
-    Matrix(Key1, Second);
-    Matrix(Key2, First);
-    Matrix(Key3, Fourth);
-    Matrix(Key4, Third);
-  End;
-
-  OnlyLetters := '';
-  For I := 1 To Length(Original) Do
-    If (Original[I] In ['A'..'Z', ' ', #10, #13]) Then
-      OnlyLetters := OnlyLetters + Original[I];
-
-  K := 1;
-  While K <= Length(OnlyLetters) Do
-  Begin
-    If (OnlyLetters[K] >= 'A') And (OnlyLetters[K] <= 'Z') Then
+  If Correct Then
+    If Encrypt Then
     Begin
-      J := K + 1;
-      While (J <= Length(OnlyLetters)) And Not (OnlyLetters[J] In ['A'..'Z']) Do
-        Inc(J);
-
-      If J > Length(OnlyLetters) Then
-      Begin
-        OnlyLetters := OnlyLetters + 'X';
-        J := Length(OnlyLetters);
-      End;
-
-      R1 := 0; C1 := 0; R2 := 0; C2 := 0;
-
-      For Row := 0 To 4 Do
-        For Col := 0 To 4 Do
-          If First[Row, Col] = OnlyLetters[K] Then
-          Begin
-            R1 := Row;
-            C1 := Col;
-          End;
-
-      For Row := 0 To 4 Do
-        For Col := 0 To 4 Do
-          If Fourth[Row, Col] = OnlyLetters[J] Then
-          Begin
-            R2 := Row;
-            C2 := Col;
-          End;
-
-      Result := Result + Second[R1, C2] + Third[R2, C1];
-
-      K := J + 1;
+      Matrix(Key1, First);
+      Matrix(Key2, Second);
+      Matrix(Key3, Third);
+      Matrix(Key4, Fourth);
     End
     Else
     Begin
-      Result := Result + OnlyLetters[K];
-      Inc(K);
+      Matrix(Key1, Second);
+      Matrix(Key2, First);
+      Matrix(Key3, Fourth);
+      Matrix(Key4, Third);
     End;
+
+  OnlyLetters := '';
+  For I := 1 To Length(Original) Do
+    If (Original[I] In ['A' .. 'Z']) Then
+      OnlyLetters := OnlyLetters + Original[I];
+
+  If (OnlyLetters = '') Or (Not Correct) Then
+  Begin
+    ShowMessage('Îòñóòñâóþò äîïóñòèìûå ñèìâîëû A-Za-z');
+    Exit;
+  End;
+
+  If Length(OnlyLetters) Mod 2 <> 0 Then
+    OnlyLetters := OnlyLetters + 'X';
+
+  I := 1;
+  While I < Length(OnlyLetters) Do
+  Begin
+    R1 := 0;
+    C1 := 0;
+    R2 := 0;
+    C2 := 0;
+
+    For Row := 0 To 4 Do
+      For Col := 0 To 4 Do
+        If First[Row, Col] = OnlyLetters[I] Then
+        Begin
+          R1 := Row;
+          C1 := Col;
+        End;
+
+    For Row := 0 To 4 Do
+      For Col := 0 To 4 Do
+        If Fourth[Row, Col] = OnlyLetters[I + 1] Then
+        Begin
+          R2 := Row;
+          C2 := Col;
+        End;
+
+    Result := Result + Second[R1, C2] + Third[R2, C1];
+
+    I := I + 2;
   End;
 End;
 
@@ -205,48 +202,43 @@ Function TForm3.Vigenere(Original, Key: String): String;
 Const
   RussianAlphabet = 'ÀÁÂÃÄÅ¨ÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞß';
 Var
-  I, J, CharOr, CharKey, CharNew, count: Integer;
-  temp: string;
+  I, J, CharOr, CharKey, CharNew: Integer;
+  Temp, TempK: String;
 Begin
   Original := AnsiUpperCase(Original);
   Key := AnsiUpperCase(Key);
   Result := '';
-  count := 0;
-  temp := '';
+  Temp := '';
+  TempK := '';
 
-    for i := 1 to Length(Original) Do
-      If (Pos(Original[I], RussianAlphabet) > 0) Or (Original[I] = #10) Or
-          (Original[I] = #13) Or
-          (Original[I] = ' ') Then
-        Temp := Temp + Original[I];
+  For I := 1 To Length(Original) Do
+    If (Pos(Original[I], RussianAlphabet) > 0) Then
+      Temp := Temp + Original[I];
 
-    For I := 1 To Length(Key) Do
-      If Pos(Key[i], RussianAlphabet) > 0 then
-      Inc(count);
+  For I := 1 To Length(Key) Do
+    If (Pos(Key[I], RussianAlphabet) > 0) Then
+      TempK := TempK + Key[I];
 
-  if count = 0 then
-    exit(temp);
+  If (Temp = '') Or (TempK = '') Then
+  Begin
+    ShowMessage('Îòñóòñâóþò äîïóñòèìûå ñèìâîëû À-ßa-ÿ');
+    Exit;
+  End;
 
   J := 1;
-  For I := 1 To Length(temp) Do
-  if Pos(temp[I], RussianAlphabet) > 0 then
+  For I := 1 To Length(Temp) Do
   Begin
-      While Pos(Key[J], RussianAlphabet) = 0 Do
-        J := (J Mod Length(Key)) + 1;
+    CharOr := Pos(Temp[I], RussianAlphabet) - 1;
+    CharKey := Pos(TempK[J], RussianAlphabet) - 1;
 
-      CharOr := Pos(temp[I], RussianAlphabet) - 1;
-      CharKey := Pos(Key[J], RussianAlphabet) - 1;
+    If Encrypt Then
+      CharNew := (CharOr + CharKey) Mod 33
+    Else
+      CharNew := (CharOr + 33 - CharKey) Mod 33;
 
-      If Encrypt Then
-        CharNew := (CharOr + CharKey) Mod 33 + 1
-      Else
-        CharNew := (CharOr + 33 - CharKey) Mod 33 + 1;
-
-      Result := Result + RussianAlphabet[CharNew];
-      J := (J Mod Length(Key)) + 1;
-  End
-  else
-    Result := Result + temp[i];
+    Result := Result + RussianAlphabet[CharNew + 1];
+    J := (J Mod Length(TempK)) + 1;
+  End;
 End;
 
 Procedure TForm3.OpenBtClick(Sender: TObject);
